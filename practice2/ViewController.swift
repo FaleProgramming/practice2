2670f85f2b1609f03fbaea78e6c7d93232bb6a08
中原孟 <hnakahara@apparray.biz>
2018-05-06 09:57:11 +0900

    Initial Commit

---
+++ practice2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice2
+//
+//  Created by 中原孟 on 2018/05/06.
+//  Copyright © 2018年 HajimeNakahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
