4ad0eebdced332831d2c9a6243332b77df25c902
中原孟 <hnakahara@apparray.biz>
2018-05-26 11:07:13 +0900

    test

---
+++ practice2/MySelf.swift
@@ -0,0 +1,22 @@
+//
+//  MySelf.swift
+//  practice2
+//
+//  Created by 中原孟 on 2018/05/06.
+//  Copyright © 2018年 HajimeNakahara. All rights reserved.
+//
+
+import UIKit
+
+class MySelf: NSObject {
+	var name: String
+	var age: Int
+	var height: Int
+	
+	init(_ name:String, _ age: Int, _ height: Int) {
+		self.name = name
+		self.age = age
+	  self.height = height
+	}
+	
+}
--- practice2/ViewController.swift
+++ practice2/ViewController.swift
@@ -8,10 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+	
+	//何行表示させるか
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+	  return 10
+	}
+	
+	//行に何を表示させるか
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = UITableViewCell(style: .value1, reuseIdentifier: "Cell")
+		
+		let myself = MySelf("えいじ", 20, 179)
+		
+		cell.textLabel?.text = myself.name
+		cell.detailTextLabel?.text = "サブタイトルです"
+		cell.imageView?.image = UIImage(named: "dog-01.png")
+		return cell
+	}
+	
+	
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 100
+	}
+	
+	
+	@IBOutlet weak var myTableView: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		myTableView.delegate = self
+		myTableView.dataSource = self
+		
+		
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
@@ -19,7 +49,30 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
+	@IBAction func tapButton(_ sender: Any) {
+		//画面遷移処理
+		let vc = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+		self.navigationController?.pushViewController(vc, animated: true)
+	}
+	
+	
 
 }
 
